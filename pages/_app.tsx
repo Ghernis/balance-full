3924fdaed53f0a5f3b5c696a5430ea58d83107d6
gherni <hernanegomez@hotmail.com>
2023-01-12 13:31:50 -0300

    install next-aut sin config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppType } from 'next/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'ar-poncho/dist/css/poncho.min.css';
 import { trpc } from '../utils/trpc';
+//import { SessionProvider } from 'next-auth/react';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType = ({
+    Component,
+    pageProps}) => {
     return(
         <>
             <Component {...pageProps} />
-            </>
+        </>
     )
 };
 export default trpc.withTRPC(MyApp);

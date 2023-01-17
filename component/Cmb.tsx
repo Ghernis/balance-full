65eaaf34d3d16051e2bada56760496f79513f2f1
gherni <hernanegomez@hotmail.com>
2023-01-17 14:40:59 -0300

    fix cuadro cmb multiples cmbs

--- component/Cmb.tsx
+++ component/Cmb.tsx
@@ -2,25 +2,31 @@ import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 
 const Cmb=()=>{
+    const tipos=['Gas Oil','Fuel Oil','Gas Natural','Carbon','Otros 1','Otros 2']
     return (
         <>
         <label>Combutible</label>
         <Table bordered hover size="sm">
             <thead>
                 <tr>
-                    <th className='text-center'>Combustible</th>
+                    <th>Combustible</th>
                     <th className='text-center'>Unidad</th>
                     <th className='text-center'>Volumen</th>
                     <th className='text-center'>Clase de Produccion</th>
                 </tr>
             </thead>
             <tbody>
-                <tr >
+                    {
+                    tipos.map(c=>{
+                    return (
+                <tr key={c}>
+                    <td>{c}</td>
                     <td><Form.Control plaintext className='text-center' type='text' placeholder='algo' /></td>
                     <td><Form.Control plaintext className='text-center' type='text' placeholder='algo' /></td>
                     <td><Form.Control plaintext className='text-center' type='text' placeholder='algo' /></td>
-                    <td><Form.Control plaintext className='text-center' type='text' placeholder='algo' /></td>
-                </tr>
+                </tr>)
+                    })
+                }
             </tbody>
         </Table>
         </>

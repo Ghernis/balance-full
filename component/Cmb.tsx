5934220823e9de2ba7e1971e5adb9b4e05e6fc3d
gherni <hernanegomez@hotmail.com>
2023-01-19 14:22:38 -0300

    implementado con handler local, falta guardar estado de los inputs

--- component/Cmb.tsx
+++ component/Cmb.tsx
@@ -1,10 +1,22 @@
+import {useState} from 'react'
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
+import TextCSV from './TextCSV';
 
 const Cmb=()=>{
     const tipos=['Gas Oil','Fuel Oil','Gas Natural','Carbon','Otros 1','Otros 2']
+    const [csv,setCsv]=useState()
+    const hand=(data)=>{
+        //console.log(data)
+        setCsv(data)
+    }
+    const changeHandler=(a)=>{
+        console.log(a)
+    }
     return (
         <>
+        <TextCSV x={3} y={6} handler={hand} />
+
         <label>Combutible</label>
         <Table bordered hover size="sm">
             <thead>
@@ -17,13 +29,22 @@ const Cmb=()=>{
             </thead>
             <tbody>
                     {
-                    tipos.map(c=>{
+                    tipos.map((c,i)=>{
                     return (
                 <tr key={c}>
                     <td>{c}</td>
-                    <td><Form.Control plaintext className='text-center' type='text' placeholder='algo' /></td>
-                    <td><Form.Control plaintext className='text-center' type='text' placeholder='algo' /></td>
-                    <td><Form.Control plaintext className='text-center' type='text' placeholder='algo' /></td>
+                    <td><Form.Control
+                                onChange={changeHandler}
+                                plaintext 
+                                value={
+                                    csv ? csv[i][0] : ''
+                                }
+                                className='text-center'
+                                type='text'
+                                placeholder='algo'
+                                /></td>
+                    <td><Form.Control onChange={changeHandler} plaintext value={csv ? csv[i][1] : ''} className='text-center' type='text' placeholder='algo' /></td>
+                    <td><Form.Control onChange={changeHandler} plaintext value={csv ? csv[i][2] : ''} className='text-center' type='text' placeholder='algo' /></td>
                 </tr>)
                     })
                 }

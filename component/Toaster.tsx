7bf5d4292e415e04e224144706e241d6f0b8f23e
Hernan Gomez <hernanegomez@gmail.com>
2023-01-28 14:19:08 -0300

    fix en toaster

--- component/Toaster.tsx
+++ component/Toaster.tsx
@@ -6,12 +6,11 @@ import Toast from 'react-bootstrap/Toast'
 import { AlertaContext } from '../context/alert.context'
 
 const Toaster=()=>{
-    const [showA,setShowA] = useState(false)
-    const {alerta} = useContext(AlertaContext)
-    const toggleShowA = () => setShowA(!showA);
+    const {alerta,setAlerta} = useContext(AlertaContext)
+    const toggleShow = () => setAlerta(!alerta);
     return(
-        <ToastContainer className="p-3" position='middle-end'>
-            <Toast show={alerta || false} onClose={toggleShowA} delay={3000} autohide>
+        <ToastContainer className="p-3" position='middle-center'>
+            <Toast show={alerta || false} onClose={toggleShow} delay={3000} autohide>
                 <Toast.Header>
                     <strong className="me-auto">Bootstrap</strong>
                     <small>11 mins ago</small>

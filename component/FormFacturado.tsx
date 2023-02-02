ce9e7cbcffc3fa94b8013862b67638a7f04c2c51
gherni <hernanegomez@hotmail.com>
2023-02-02 12:46:42 -0300

    add textarea to csv

--- component/FormFacturado.tsx
+++ component/FormFacturado.tsx
@@ -42,12 +42,33 @@ const FormFacturado=(props)=>{
 
     })
     const [csv,setCsv]=useState(data)
+    const [dataTabla,setDataTabla]=useState([[]])
     const [depaCargando, setDepaCargando]=useState(departamentos[0])
+    const [showSave,setShowSave]=useState(false)
     const [indexDepa, setIndexDepa]=useState(0)
 
     useEffect(()=>{
-        console.log(csv)
-    },[csv])
+        if(indexDepa==departamentos.length-1){
+            setShowSave(true)
+        }
+        else{
+            setShowSave(false)
+        }
+
+        if(dataTabla[0].length>0){
+            const auxCsv = csv
+            auxCsv[indexDepa].facturado.forEach((c,i)=>{
+                const inputCantU=parseInt(dataTabla[i][0])
+                const inputKwh=parseFloat(dataTabla[i][1])
+                updateTabla(inputCantU,i,'cantUser')
+                updateTabla(inputKwh,i,'kwh')
+            })
+            console.log('setcsv en useeffect')
+            console.log(auxCsv)
+            //setCsv(auxCsv)
+        }
+
+    },[indexDepa,dataTabla])
     const nextDepa=()=>{
         if(indexDepa<departamentos.length-1){
             setIndexDepa(indexDepa+1)
@@ -58,15 +79,33 @@ const FormFacturado=(props)=>{
             setIndexDepa(indexDepa-1)
         }
     }
-    const hand=(data)=>{
-        console.log(data)
-        setCsv(data)
+    const updateTabla=(input:number,i:number,tipo:string)=>{
+        let aux=[...csv]
+        if(isNaN(input)){
+            input=0
+        }
+        if(tipo=='cantUser'){
+            aux[indexDepa].facturado[i].cantUser=input
+        }
+        else{
+            aux[indexDepa].facturado[i].kwh=input
+        }
+        setCsv(aux)
     }
+    const hand=(data)=>{
+        setDataTabla(data)
+        console.log(dataTabla)
+    }
+    const saveFacturacion=()=>{
+        console.log(csv)
+    }
+    
+
     return (
         <div className='container'>
-            <TextCSV x={2} y={10} handler={hand} />
+            <TextCSV x={2} y={10} handler={setDataTabla} />
             <h4 className='mt-4'>Facturado en departamento {indexDepa+1}/{departamentos.length}: {departamentos[indexDepa].departamento}</h4>
-            <label>Usar '.' como decimal</label>
+            <label>Usar '.' como punto decimal</label>
             <Table bordered hover size="sm">
                 <thead>
                     <tr>
@@ -88,26 +127,16 @@ const FormFacturado=(props)=>{
                                 <td>{c.concepto}</td>
                                 <td><Form.Control 
                                         onChange={(e)=>{
-                                            let aux=[...csv]
-                                            let input:number = parseInt(e.target.value)
-                                            if(isNaN(input)){
-                                                input=0
-                                            }
-                                            aux[indexDepa].facturado[i].cantUser=input
-                                            setCsv(aux)
+                                            const input:number = parseInt(e.target.value) 
+                                            updateTabla(input,i,'cantUser')
                                         }}
                                         value={c.cantUser} plaintext className='text-center' type='number' placeholder='' 
                                     />
                                 </td>
                                 <td><Form.Control 
                                         onChange={(e)=>{
-                                            let aux=[...csv]
-                                            let input:number=parseFloat(e.target.value)
-                                            if(isNaN(input)){
-                                                input=0
-                                            }
-                                            aux[indexDepa].facturado[i].kwh=input
-                                            setCsv(aux)
+                                            const input:number = parseFloat(e.target.value) 
+                                            updateTabla(input,i,'kwh')
                                         }}
                                         value={c.kwh} plaintext className='text-center' type='number' placeholder='' /></td>
                                 {/*
@@ -125,8 +154,13 @@ const FormFacturado=(props)=>{
                 <div className='col'>
                     <Button onClick={()=>prevDepa()}>Anterior</Button>
                 </div>
-                <div className='col'>
+                <div className='col text-center'>
                     <Button onClick={()=>nextDepa()}>Siguiente</Button>
+                </div>
+                <div className='col text-end'>
+                    {
+                        showSave && <Button variant='success'  onClick={()=>saveFacturacion()}>Guardar Facturacion</Button>
+                    }
                 </div>
             </div>
         </div>
--- component/TextCSV.tsx
+++ component/TextCSV.tsx
@@ -3,9 +3,11 @@ import {useState,useEffect} from 'react';
 import Form from 'react-bootstrap/Form';
 
 const TextCSV=(props)=>{
-    const { x, y,handler } = props
+    const { x, y, handler } = props
     const [data,setData]=useState('')
-    const [pros, setPros]=useState('')
+    const [proce,setProce]=useState([[]])
+    //const [pros, setPros]=useState('')
+    
     useEffect(()=>{
         const lines = data.trimEnd().split(/\r?\n/);
         let res:any[] = []
@@ -13,23 +15,22 @@ const TextCSV=(props)=>{
             const cols =l.split('\t') 
             res.push(cols)
         })
-        //console.log(res)
         if(!res || res.length != y || res[0].length != x){
             console.log('mal'+res.length+' '+res[0].length)
         }
         else{
+            //setProce(res)
+            console.log('paso res por handler')
             handler(res)
         }
-        setPros(data)
-
-
-    },[data])
+        //setPros(data)
+    },[data,proce])
 
     return (
         <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Copia y Pega desde excel</Form.Label>
-                <Form.Control as="textarea" rows={3} className='h-100' onChange={(e)=>setData(e.target.value)}/>
+                <Form.Control as="textarea" rows={4} className='h-100' onChange={(e)=>setData(e.target.value)}/>
             </Form.Group>
         </Form>
     )
